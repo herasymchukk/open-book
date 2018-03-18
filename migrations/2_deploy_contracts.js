d67d536a7e48c077298a45ce1a276620c5372134
Taras Herasymchuk <taras.herasymchuk@gmail.com>
2018-03-18 18:49:36 +0200

    fixed indentation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const Ownable = artifacts.require("Ownable.sol");
 const AssetContract = artifacts.require("AssetContract.sol");
 
 module.exports = async (deployer) => {
-    deployer.deploy(Util);
-    deployer.deploy(Ownable);
-    deployer.deploy(AssetContract, "book_id", 1000000, ["0x0000000000000000000000000000000000000001", "0x0000000000000000000000000000000000000002"]);
+	deployer.deploy(Util);
+	deployer.deploy(Ownable);
+	deployer.deploy(AssetContract, "book_id", 1000000, ["0x0000000000000000000000000000000000000001", "0x0000000000000000000000000000000000000002"]);
 };
